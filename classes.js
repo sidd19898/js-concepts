f92358abe76812c141af13a17649b3d7f7a44960
sidd19898 <anganesiddhu@gmail.com>
2025-10-17 08:50:06 +0530

    Add Employee class with static total employees counter

---
+++ classes.js
@@ -0,0 +1,24 @@
+class Employee{
+    constructor(name,position,salary){
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+
+        Employee.totalemployees++;
+    }
+    details(){
+        console.log(`${this.name} works as a ${this.position} and earns ${this.salary}.`);
+    }
+    static total(){
+        console.log(`Total Employees:${Employee.totalemployees}`);
+    }
+}
+
+Employee.totalemployees = 0;
+
+let employee1 = new Employee("Alice","Manager",50000);
+let employee2 = new Employee("Mark","co-founder",10000000);
+employee1.details();
+employee2.details();
+Employee.total();
+
